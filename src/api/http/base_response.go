8d8cb1dc65064146fae62beb62ae8ae9e3b04632
hossein <hosseinrouhi79@gmail.com>
2024-02-13 12:29:01 +0000

    create validation error

--- src/api/http/base_response.go
+++ src/api/http/base_response.go
@@ -1,9 +1,11 @@
 package http
 
+import "github.com/HosseinRouhi79/golang-clean-web-api/src/api/validation"
+
 type HTTPResponse struct {
-	Result           string             `json:"result"`
-	Success          string             `json:"success"`
-	ResultCode       string             `json:"resultCode"`
-	ValidationErrors *[]ValidationError `json:"validationErrors"`
-	Error            any                `json:"error"`
+	Result           string                        `json:"result"`
+	Success          string                        `json:"success"`
+	ResultCode       string                        `json:"resultCode"`
+	ValidationErrors *[]validation.ValidationError `json:"validationErrors"`
+	Error            any                           `json:"error"`
 }
