3943477cb55b5b6b06015822d6b4dfee8a5a0783
hossein <hosseinrouhi79@gmail.com>
2024-02-19 06:40:48 +0000

    create zerologger

--- src/pkg/logging/zero_logger.go
+++ src/pkg/logging/zero_logger.go
@@ -1,3 +1,55 @@
 package logging
 
-//TODO ZEROLOG CONFIG
+import (
+	"github.com/HosseinRouhi79/golang-clean-web-api/src/config"
+	"github.com/rs/zerolog"
+)
+
+type zerologger struct {
+	cfg    *config.Config
+	logger *zerolog.Logger
+}
+
+var zeroFinLogger *zerologger
+
+var zerologMap = map[string]zerolog.Level{
+	"debug": zerolog.DebugLevel,
+	"info":  zerolog.InfoLevel,
+	"warn":  zerolog.WarnLevel,
+	"error":  zerolog.ErrorLevel,
+	"fatal": zerolog.FatalLevel,
+}
+
+func (l *zerologger) getLevel(*zerologMap map[string]zerolog.Level) zerolog.Level {
+	level, ok := zerologMap[l.cfg.Logger.Level]
+	if !ok {
+		return zerologMap.DebugLevel
+	}
+	return level
+}
+
+func newZeroLogger(cfg zerolog.Config) zerologger{
+	zero := &zerologger{cfg: cfg}
+	zero.Init()
+	return zero
+}
+
+func (zerologger *zerologger) Init() {
+
+}
+
+func Debug(message string) {}
+
+func Debugf(message string) {}
+
+func Info(message string) {}
+
+func Infof(message string) {}
+
+func Fatal(message string) {}
+
+func Fatalf(message string) {}
+
+func Warn(message string) {}
+
+func Warnf(message string) {}
