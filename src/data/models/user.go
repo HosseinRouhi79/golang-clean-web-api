36f7ab7db55c28507f71f91d9f8044d0ade8306f
hossein <hosseinrouhi79@gmail.com>
2024-04-07 11:09:06 +0000

    add user & roles table

--- src/data/models/user.go
+++ src/data/models/user.go
@@ -2,7 +2,11 @@ package models
 
 type User struct {
 	BaseModel
-	FirstName string `gorm:"not null"`
-	LastName  string `gorm:"not null"`
+	UserName  string `gorm:"type:string; not null; unique; size:20"`
+	FirstName string `gorm:"not null; size:15; type:string"`
+	LastName  string `gorm:"not null; size:25; type:string"`
+	Mobile string `gorm:"null; size:11; default:null; type:string"`
+	Password string `gorm:"not null; size:64; type:string"`
+	Email string `gorm:"not null; size:64; type:string"`
+	Enabled bool `gorm:"default:true"`
 }
-
