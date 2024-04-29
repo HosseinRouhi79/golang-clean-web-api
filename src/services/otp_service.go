9ffc6d3c3eaf06a0576109fe5785beae4605b4ae
hossein <hosseinrouhi79@gmail.com>
2024-04-29 09:32:00 +0000

    revalidate the otp token

--- src/services/otp_service.go
+++ src/services/otp_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
@@ -29,11 +30,23 @@ func NewOtpService(cfg *config.Config) *OtpService {
 }
 
 func (otpService *OtpService) SetOtp(mobile string, otp string) error {
-	
+
 	var prefix string = "redis"
 	key := fmt.Sprintf("%s:%s", prefix, mobile)
 	val := OtpCred{Value: otp, Used: false}
-	err := cache.Set[OtpCred](otpService.redisClient, key, val, time.Second*3600)
+
+	dest, err := cache.Get[OtpCred](otpService.redisClient, key)
+
+	if err == nil && dest.Used {
+		errMessage := errors.New("the otp is used")
+		return errMessage
+	} else if err == nil && !dest.Used {
+		errMessage := errors.New("the otp exists")
+		return errMessage
+	}
+
+	err = cache.Set[OtpCred](otpService.redisClient, key, val, time.Second*3600)
+
 	if err != nil {
 		return err
 	}
@@ -41,6 +54,27 @@ func (otpService *OtpService) SetOtp(mobile string, otp string) error {
 }
 
 func (otpService *OtpService) ValidateOtp(mobile string, otp string) error {
-	//TODO: implement validateOtp
+	var prefix string = "redis"
+	key := fmt.Sprintf("%s:%s", prefix, mobile)
+
+	dest, err := cache.Get[OtpCred](otpService.redisClient, key)
+
+	if err != nil {
+		return err
+	} else if dest.Used {
+		errMessage := errors.New("the otp is used")
+		return errMessage
+	} else if !dest.Used && dest.Value != otp {
+		errMessage := errors.New("the otp is not valid")
+		return errMessage
+	} else if !dest.Used && dest.Value == otp {
+		dest.Used = true
+		err = cache.Set[OtpCred](otpService.redisClient, key, dest, time.Second*3600)
+
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
