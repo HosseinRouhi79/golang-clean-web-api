7778b7bb2483a84d8962ce57d56bb5bea9021b83
hossein <hosseinrouhi79@gmail.com>
2024-06-26 11:20:07 +0000

    fix authentication middleware and apply it

--- src/api/middlewares/auth.go
+++ src/api/middlewares/auth.go
@@ -2,13 +2,13 @@ package middlewares
 
 import (
 	"fmt"
+	"net/http"
 	"strings"
 
 	"github.com/HosseinRouhi79/golang-clean-web-api/src/config"
 	"github.com/HosseinRouhi79/golang-clean-web-api/src/constants"
 	"github.com/HosseinRouhi79/golang-clean-web-api/src/services"
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt"
 )
 
 func Authentication(cfg *config.Config) gin.HandlerFunc {
@@ -25,43 +25,42 @@ func Authentication(cfg *config.Config) gin.HandlerFunc {
 
 			claimsMap, err = tokenService.GetClaims(accessToken)
 			if err != nil {
-				switch err.(*jwt.ValidationError).Errors {
-				case jwt.ValidationErrorExpired:
-					err = fmt.Errorf("token is expired")
-				default:
-					err = fmt.Errorf("error has occured")
-				}
+				err = fmt.Errorf("error has occured:%s", err.Error())
 			}
 		}
-		if err == nil {
-			c.Set("id", claimsMap["id"])
-			c.Set("userName", claimsMap["userName"])
-			c.Set("firstName", claimsMap["firstName"])
-			c.Set("lastName", claimsMap["lastName"])
-			c.Set("mobileNumber", claimsMap["mobileNumber"])
-			c.Set("email", claimsMap["email"])
-			c.Set("roles", claimsMap["roles"])
-			c.Set("exp", claimsMap["exp"])
-
-			c.Next()
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"status": false, "message": err.Error()})
+			return
 		}
+
+		c.Set("id", claimsMap["id"])
+		c.Set("userName", claimsMap["userName"])
+		c.Set("firstName", claimsMap["firstName"])
+		c.Set("lastName", claimsMap["lastName"])
+		c.Set("mobileNumber", claimsMap["mobileNumber"])
+		c.Set("email", claimsMap["email"])
+		c.Set("roles", claimsMap["roles"])
+		c.Set("exp", claimsMap["exp"])
+
+		c.Next()
+
 	}
 }
 
-func Authorization(validRoles []string) gin.HandlerFunc{
+func Authorization(validRoles []string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var err error
 		var roleList []string
 		roleval, ok := c.Keys["roles"]
-		if !ok{
+		if !ok {
 			err = fmt.Errorf("error has occured")
-		} else{
+		} else {
 			roleList = append(roleList, roleval.([]string)...)
 		}
-		if err == nil{
-			for _, role := range roleList{
-				for _, vRole := range validRoles{
-					if role == vRole{
+		if err == nil {
+			for _, role := range roleList {
+				for _, vRole := range validRoles {
+					if role == vRole {
 						c.Next()
 					}
 				}
--- src/api/routers/auth.go
+++ src/api/routers/auth.go
@@ -2,14 +2,17 @@ package routers
 
 import (
 	"github.com/HosseinRouhi79/golang-clean-web-api/src/api/handlers"
+	"github.com/HosseinRouhi79/golang-clean-web-api/src/api/middlewares"
+	"github.com/HosseinRouhi79/golang-clean-web-api/src/config"
 	"github.com/gin-gonic/gin"
 )
 
 func Auth(r *gin.RouterGroup) {
+	cfg := config.GetConfig()
 	handler := handlers.AuthMobile{}
 	tokenHandler := handlers.TokenHandler{}
 	r.POST("/register-login-mobile", handler.RLMobile)
-	r.POST("/claim", tokenHandler.GetClaims)
+	r.POST("/claim",middlewares.Authentication(cfg), tokenHandler.GetClaims)
 	// r.POST("/register", handler.Register)
 	// r.POST("/logout", handler.Logout)
 	// r.POST("/refresh", handler.Refresh)
