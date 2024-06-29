bd2f5e561e3676fb0b2a4b36ec690528342cdece
hossein <hosseinrouhi79@gmail.com>
2024-06-29 12:11:30 +0000

    update country service

--- src/services/country_service.go
+++ src/services/country_service.go
@@ -2,6 +2,8 @@ package services
 
 import (
 	"context"
+	"database/sql"
+	"time"
 
 	"github.com/HosseinRouhi79/golang-clean-web-api/src/config"
 	"github.com/HosseinRouhi79/golang-clean-web-api/src/data/db"
@@ -27,6 +29,8 @@ func NewCountryService(cfg *config.Config) *CountryService {
 // create
 func (cs *CountryService) Create(ctx context.Context, req *dto.CreateUpdateCountry) (res *dto.CountryResponse, err error) {
 	country := models.Country{Name: req.Name}
+	country.CreatedBy = int(ctx.Value("id").(float64))
+	country.CreatedAt = time.Now().UTC()
 	tx := cs.DB.WithContext(ctx).Begin()
 
 	err = tx.Create(&country).Error
@@ -34,6 +38,7 @@ func (cs *CountryService) Create(ctx context.Context, req *dto.CreateUpdateCount
 	if err != nil {
 		tx.Rollback()
 		cs.Logger.Info(logging.Postgres, logging.Insert, err.Error(), nil)
+		return nil, err
 	}
 	tx.Commit()
 	res = &dto.CountryResponse{
@@ -44,6 +49,32 @@ func (cs *CountryService) Create(ctx context.Context, req *dto.CreateUpdateCount
 }
 
 //update
+
+func (cs *CountryService) Update(ctx context.Context, req *dto.CreateUpdateCountry, id int) (res *dto.CountryResponse, err error) {
+
+	//create updateMap
+	updateMap := map[string]interface{}{
+		"Name":        req.Name,
+		"Modified_by": sql.NullInt64{Int64: int64(ctx.Value("id").(float64)), Valid: true},
+		"Modified_at": sql.NullTime{Time: time.Now().UTC(), Valid: true},
+	}
+
+	tx := cs.DB.WithContext(ctx).Begin()
+	err = tx.
+		Model(models.Country{}).
+		Where("id = ?", id).
+		Updates(updateMap).
+		Error
+
+	if err != nil {
+		tx.Rollback()
+		cs.Logger.Info(logging.Postgres, logging.Update, err.Error(), nil)
+		return nil, err
+	}
+
+	tx.Commit()
+
+}
 
 //delete
 
